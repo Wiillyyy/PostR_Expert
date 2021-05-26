f99ade016814a8690d2ebdeb15f3db2729b22a11
Wiillyyy <luigi385@gmail.com>
2021-05-26 20:21:23 +0200

    Premier Commit :)

---
+++ 2-UserCo/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="style.css">
+    </head>
+<div class="wrapper fadeInDown">
+    <div id="formContent">
+      <!-- Se connecter / -->
+      <h2 class="active"> Se Connecter </h2>
+      <h2 class="inactive underlineHover"> <a href="Inscription.php">Inscription</a></h2>
+  
+  
+      <!-- Section Login -->
+      <form>
+        <input type="text" id="login" class="fadeIn second" name="Email" placeholder="Email">
+        <input type="text" id="password" class="fadeIn third" name="Mot De Passe" placeholder="Mot de Passe">
+        <input type="submit" class="fadeIn fourth" value="Connexion">
+      </form>
+  
+      <!-- Remind Passowrd -->
+      <div id="formFooter">
+        <a class="underlineHover" href="#">Mot de passe oublié ?</a>
+      </div>
+  
+    </div>
+  </div>
+  </html>
---
+++ java.js
@@ -0,0 +1,12 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+
+
+typewriter.typeString('Bienvenue !')
+    .pauseFor(2500)
+    .deleteAll()
+    .start();
